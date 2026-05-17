5c2973596418ba37bd5fbac1405719def6617d9b
Nguyễn Văn Hòa <cloudris0267@gmail.com>
2026-05-17 14:08:16 +0700

    v4

---
+++ now2_v4/viterbi/xcelium.d/run.lnx8664.25.09.d/files.ts
@@ -0,0 +1,8 @@
+1778995852 /home/iclab6/Desktop/viterbi/rtl/control.v
+1778995742 /home/iclab6/Desktop/viterbi/rtl/extract_bit.v
+1778995843 /home/iclab6/Desktop/viterbi/rtl/branch_metric.v
+1778995831 /home/iclab6/Desktop/viterbi/rtl/add_comp_slt.v
+1778995886 /home/iclab6/Desktop/viterbi/rtl/traceback.v
+1778995731 /home/iclab6/Desktop/viterbi/rtl/memory.v
+1778995896 /home/iclab6/Desktop/viterbi/rtl/viterbi_decoder.v
+1778984371 /home/iclab6/Desktop/viterbi/tb/tb_viterbi_readmemb.v
